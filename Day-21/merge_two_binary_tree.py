19589f24ba8ed364776c356419c3345d0e7eb537
Shaista Afreen <97823364+ShaistaAfreen09@users.noreply.github.com>
2024-08-03 19:29:47 +0530

    Add files via upload

---
+++ Day-21/merge_two_binary_tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root1:
+            return root2
+        queue = deque([(root1,root2)])
+        while queue:
+            node1, node2 = queue.popleft()
+            if not node1 or not node2:
+                continue
+            node1.val = node1.val + node2.val
+            if not node1.left:
+                node1.left = node2.left
+            else:
+                queue.append((node1.left, node2.left))
+            if not node1.right:
+                    node1.right = node2.right
+            else:
+                queue.append((node1.right, node2.right))
+
+        return root1
+
